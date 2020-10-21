0c6ee0fff2b10531205b0637a459d303a95e7875
sajal singhal <sajalsinghal83@gmail.com>
2020-10-21 13:04:55 +0530

    adding simplesocial project

---
+++ simplesocial/groups/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from django.conf.urls import url
+from . import views
+
+app_name = 'groups'
+
+urlpatterns=[
+    path('', views.ListGroups.as_view(), name="all"),
+    path('new/', views.CreateGroup.as_view(), name="create"),
+
+    path('posts/in/<slug>/', views.SingleGroup.as_view(), name="single"),
+    path('join/<slug>/', views.JoinGroup.as_view(), name="join"),
+    path('leave/<slug>/', views.LeaveGroup.as_view(), name="leave")
+
+    # url(r'posts/in/(?P<slug>[-\w]+)/$', views.SingleGroup.as_view(), name="single"),
+    # url(r'join/(?P<slug>[-\w]+)/$', views.JoinGroup.as_view(), name="join"),
+    # url(r'leave/(?P<slug>[-\w]+)/$', views.LeaveGroup.as_view(), name="leave")
+]
